900b078fb02c8c9285d5eb2a09015d7f83910f16
Benjamin Geißler <me@web-ben.de>
2022-07-07 14:13:17 +0200

    feat(logging): add morgan for better call logs

--- apps/dev-server/src/app/app.module.ts
+++ apps/dev-server/src/app/app.module.ts
@@ -4,14 +4,17 @@ import { CenterMatrixModule } from '@tss/ctrm-backend';
 import { CoreModule } from '../core/core.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { MorganModule, MorganInterceptor } from 'nest-morgan';
 @Module({
-  imports: [
-   
-    CoreModule,
-    CenterMatrixModule
-  ],
+  imports: [CoreModule, CenterMatrixModule, MorganModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: MorganInterceptor('tiny'),
+    },
+  ],
 })
-export class AppModule { }
+export class AppModule {}
