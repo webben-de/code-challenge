4ec5e050453357836412b318d09e3e1c6f7645d6
Benjamin Geißler <me@web-ben.de>
2022-07-07 16:14:18 +0200

    feat: prepare way to solution

--- apps/developer-app/src/app/app.routing.module.ts
+++ apps/developer-app/src/app/app.routing.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
+import { NxWelcomeComponent } from './nx-welcome.component';
 
 const routes: Routes = [
-  { path: '', component: AppComponent },
+  { path: 'welcome', component: NxWelcomeComponent },
   {
     path: 'centers',
     loadChildren: () =>
       import('./centers/centers.module').then((m) => m.CentersModule),
   },
-  { path: '**', component: AppComponent },
+  { path: '**', component: NxWelcomeComponent },
 ];
 
 @NgModule({
