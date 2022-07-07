4f07fe4c620f31919b522d1ecf406a991abf2b6f
Benjamin Geißler <me@web-ben.de>
2022-07-07 14:15:45 +0200

    feat(dev-app): add Router to untouch Core Component - add lazy load route "/centers" - add http Module

--- apps/developer-app/src/app/app.module.ts
+++ apps/developer-app/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
-
+import { AppRoutingModule } from './app.routing.module';
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ apps/developer-app/src/app/app.routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+
+const routes: Routes = [
+  { path: '', component: AppComponent },
+  {
+    path: 'centers',
+    loadChildren: () =>
+      import('./centers/centers.module').then((m) => m.CentersModule),
+  },
+  { path: '**', component: AppComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
